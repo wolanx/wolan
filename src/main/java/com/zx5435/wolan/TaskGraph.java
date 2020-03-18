a2d646320629d454b75325cace7b3fac82a47cbd
yujie <825407762@qq.com>
2020-03-18 21:53:47 +0800

    init task model

--- src/main/java/com/zx5435/wolan/TaskGraph.java
+++ src/main/java/com/zx5435/wolan/TaskGraph.java
@@ -1,10 +1,17 @@
 package com.zx5435.wolan;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.zx5435.wolan.model.TaskDO;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Component;
+import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -13,22 +20,31 @@ import java.util.Objects;
 @Component
 public class TaskGraph implements GraphQLQueryResolver {
 
-    public TaskDO getTaskByName(String name) {
-        return null;
+    private static final String WorkPath = "./src/main/resources/gitops";
+
+    public TaskDO getTaskByName(String taskName) throws FileNotFoundException {
+        Yaml yaml = new Yaml();
+        File woFile = new File(WorkPath + "/" + taskName + "/wolan.yaml");
+        Object obj = yaml.load(new FileInputStream(woFile));
+
+        ObjectMapper mapper = new ObjectMapper();
+        TaskDO task = mapper.convertValue(obj, TaskDO.class);
+
+        System.out.println("task = " + task);
+        return task;
     }
 
-    public List<TaskDO> listTask() {
+    public List<TaskDO> listTask() throws IOException {
         ArrayList<TaskDO> res = new ArrayList<>();
-        System.out.println("res = " + res);
 
-        File file = new File("./src/main/resources/gitops");
-        File[] files = file.listFiles();
+        File workFile = new File(WorkPath);
+        File[] taskFiles = workFile.listFiles();
 
-        for (File task : Objects.requireNonNull(files)) {
-            if (task.isDirectory()) {
-                TaskDO taskDO = new TaskDO();
-//                taskDO
-                res.add(taskDO);
+        for (File taskFile : Objects.requireNonNull(taskFiles)) {
+            if (taskFile.isDirectory()) {
+                String taskName = taskFile.getName();
+                TaskDO task = getTaskByName(taskName);
+                res.add(task);
             }
         }
 
--- src/main/java/com/zx5435/wolan/model/TaskDO.java
+++ src/main/java/com/zx5435/wolan/model/TaskDO.java
@@ -1,10 +1,29 @@
 package com.zx5435.wolan.model;
 
+
 import lombok.Data;
 
 @Data
 public class TaskDO {
 
+    /**
+     * version: "1.0"
+     * name: "go-fs"
+     * git:
+     * url: 'https://github.com/zx5435/go-fs.git'
+     * branch: 'master'
+     * docker-compose: "docker-composer.yml"
+     */
+
+    private String version;
     private String name;
+
+    private GitBean git;
+
+    @Data
+    private static class GitBean {
+        private String url;
+        private String branch;
+    }
 
 }
