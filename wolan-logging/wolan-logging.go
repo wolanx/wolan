2b93b48194611d2bd9880c379448174b2265c0c7
yujie <825407762@qq.com>
2021-11-25 22:25:20 +0800

    proxy

--- wolan-logging/wolan-logging.go
+++ wolan-logging/wolan-logging.go
@@ -2,44 +2,37 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"net/http/httputil"
 )
 
+import "github.com/gin-gonic/gin"
+
 func main() {
-	http.HandleFunc("/", sayHello)
-	http.HandleFunc("/_bulk", _bulk)
-	err := http.ListenAndServe(":20100", nil)
+	r := gin.Default()
+
+	r.NoRoute(proxyHandle)
+	r.GET("/ping", pingHandle)
+	r.GET("/login", proxyHandle)
+
+	err := r.Run(":20100")
 	if err != nil {
-		log.Fatal(err)
+		return
 	}
 }
 
-// {
-//  "name" : "dep-elasticsearch-75f55c6b49-8tfjj",
-//  "cluster_name" : "docker-cluster",
-//  "cluster_uuid" : "1LRsCfEsS-O0DU1nYwOXsg",
-//  "version" : {
-//    "number" : "7.12.0",
-//    "build_flavor" : "default",
-//    "build_type" : "docker",
-//    "build_hash" : "78722783c38caa25a70982b5b042074cde5d3b3a",
-//    "build_date" : "2021-03-18T06:17:15.410153305Z",
-//    "build_snapshot" : false,
-//    "lucene_version" : "8.8.0",
-//    "minimum_wire_compatibility_version" : "6.8.0",
-//    "minimum_index_compatibility_version" : "6.0.0-beta1"
-//  },
-//  "tagline" : "You Know, for Search"
-//}
-func sayHello(w http.ResponseWriter, r *http.Request) {
-	l := r.ContentLength
-	body := make([]byte, l)
+func pingHandle(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
 
-	_, _ = r.Body.Read(body)
-	w.Header().Set("Content-Type", "application/json")
-	_, _ = w.Write([]byte(`{"version":{"number":"7.12.0"}}`))
-	fmt.Println(r.Method, r.URL, string(body))
+func proxyHandle(c *gin.Context) {
+	proxy := &httputil.ReverseProxy{Director: func(req *http.Request) {
+		req.URL.Scheme = "http"
+		req.URL.Host = "svc-es:9200"
+	}}
+	proxy.ServeHTTP(c.Writer, c.Request)
 }
 
 func _bulk(w http.ResponseWriter, r *http.Request) {
